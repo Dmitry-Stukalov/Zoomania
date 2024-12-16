99c624327df4cb4eafecac2841fbb8a125585557
Dmitry-Stukalov <dimastukalov04@gmail.com>
2024-12-16 23:58:07 +0300

    Add map

--- Assets/Zoomania/UI/BuyRegion.cs
+++ Assets/Zoomania/UI/BuyRegion.cs
@@ -6,10 +6,15 @@ using UnityEngine.EventSystems;
 public class BuyRegion : MonoBehaviour, IPointerClickHandler
 {
 	public GameObject BuyMenu;
+	public GameObject Map;
 	public bool IsBuy;
 	public void OnPointerClick(PointerEventData data)
 	{
 		if (!IsBuy) BuyMenu.SetActive(true);
-		else Debug.Log("Этот регион уже куплен");
+		else
+		{
+			Map.SetActive(false);
+			Debug.Log("Этот регион уже куплен");
+		}
 	}
 }
