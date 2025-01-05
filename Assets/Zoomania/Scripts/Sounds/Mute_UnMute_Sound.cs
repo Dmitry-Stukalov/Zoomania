158f373ba1e6027fa0d3f6e4680dd32f76d8dc3a
Dmitry-Stukalov <dimastukalov04@gmail.com>
2025-01-05 13:02:56 +0300

    Fix Music and Sound Buttons (now bug with music)

---
+++ Assets/Zoomania/Scripts/Sounds/Mute_UnMute_Sound.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Mute_UnMute_Sound : MonoBehaviour, IPointerClickHandler
+{
+	[field: SerializeField] private Sprite Mute {  get; set; }
+	[field: SerializeField] private Sprite UnMute { get; set; }
+	private bool IsMute { get; set; } = false;
+
+	void Start()
+	{
+		gameObject.GetComponent<Image>().sprite = UnMute;
+	}
+
+	public void OnPointerClick(PointerEventData data)
+	{
+		if (!IsMute)
+		{
+			foreach (var audio in GameObject.FindGameObjectsWithTag("Audio"))
+				audio.GetComponent<AudioSource>().mute = true;
+
+			gameObject.GetComponent<Image>().sprite = Mute;
+
+			IsMute = true;
+		}
+		else
+		{
+			foreach (var audio in GameObject.FindGameObjectsWithTag("Audio")) 
+				audio.GetComponent<AudioSource>().mute = false;
+
+			gameObject.GetComponent<Image>().sprite = UnMute;
+
+			IsMute = false;
+		}
+	}
+}
